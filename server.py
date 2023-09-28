cce082f155444c53c34dc61ee3037cc09803f62a
TekatoKiary <mineevdanis@mail.ru>
2023-09-28 20:31:11 +0300

    Create base html

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key'
 
+
 @app.route('/')
 def index():
-    pass
+    return render_template('base.html')
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0')
